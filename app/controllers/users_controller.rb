22124b6744b1137f473c74306ea37ecdca314fbd
cep104 <candicelizabethpeters@gmail.com>
2020-05-26 20:00:11 -0500

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+    def welcome
+        @user = current_user
+    end
+    
+    
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to '/'
+            session[:user_id] = @user.id
+        else
+            redirect_to signup_path
+        end
+
+    end
+
+    private 
+    def user_params
+        params.require(:user).permit(:name, :password, :password_confirmation)
+    end
+end
